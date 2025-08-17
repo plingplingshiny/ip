dbae86c5e76b0e1e6181e08b82e145370bcbe239
plingplingshiny <226284094+plingplingshiny@users.noreply.github.com>
2025-08-18 01:04:07 +0800

    Level-0: Rename, Greet, Exit
    Rename chatbot and add greeting and exit messages

---
+++ src/main/java/Nila.java
@@ -0,0 +1,12 @@
+public class Nila {
+    public static void main(String[] args) {
+        String horizontalLine = "____________________________________________________________";
+        String name = "Nila";
+        System.out.println(horizontalLine);
+        System.out.println("Hello! I'm " + name + " :)");
+        System.out.println("What can I do for you today?");
+        System.out.println(horizontalLine);
+        System.out.println("Bye! Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}

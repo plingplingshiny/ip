ccf3ab93bf25a71e91a740c4fe48658df636a7f7
plingplingshiny <226284094+plingplingshiny@users.noreply.github.com>
2025-09-17 22:36:51 +0800

    Add help manual to GUI
    Add a 'help' command which will display a list of commands that the Nila
    chatbot accepts, the format of the commands and some examples to help
    the user.

--- src/main/java/nila/Nila.java
+++ src/main/java/nila/Nila.java
@@ -24,7 +24,7 @@ public class Nila {
      * Represents the different types of commands supported by the {@code Nila} chatbot
      */
     public enum Command {
-        LIST, MARK, UNMARK, DELETE, TODO, DEADLINE, EVENT, BYE, UNKNOWN, FIND
+        LIST, MARK, UNMARK, DELETE, TODO, DEADLINE, EVENT, BYE, UNKNOWN, FIND, HELP
     }
 
     /**
@@ -72,6 +72,8 @@ public class Nila {
             return taskList.findTasksAsString(Parser.parseFind(remaining));
         case BYE:
             return "Goodbye!";
+        case HELP:
+            return displayHelp();
         case UNKNOWN:
             return "Sorry, I don't understand: " + rawInput;
         default:
@@ -79,9 +81,13 @@ public class Nila {
         }
     }
 
-    /** Returns the greeting message for GUI display */
+    /**
+     * Returns the greeting message for GUI display
+     */
     public String getGreeting() {
-        return "Hello! I'm Nila \uD83E\uDD81\nWhat can I do for you today?\n";
+        return "Hello! I'm Nila \uD83E\uDD81\n"
+                + "What can I do for you today?\n"
+                + "Type 'help' for more info!";
     }
 
     /**
@@ -90,5 +96,63 @@ public class Nila {
      */
     public String getGoodbye() {
         return "Bye!\uD83D\uDC4B Hope to see you again soon!";
+    }
+
+    /**
+     * Returns a help message for GUI display
+     * @return a help manual listing all the commands, their uses and format
+     */
+    public String displayHelp() {
+        StringBuilder sb = new StringBuilder("\u2753HELP MANUAL\n");
+        sb.append(displayListHelp()).append(displayTaskHelp());
+        sb.append(displayMarkHelp()).append(displayUnmarkHelp()).append(displayDeleteHelp());
+        sb.append(displayFindHelp()).append(displayByeHelp());
+        sb.append("HELP\ndisplay this help manual\n\n"
+                + "Format: help");
+        return sb.toString();
+    }
+
+    private String displayListHelp() {
+        return "\nLIST\n • shows a list of tasks added\n\n"
+                + "Format: list\n\n";
+    }
+
+    private String displayTaskHelp() {
+        return "TASKS\n • we have 3 different task types: Todo, Deadline, Event\n\n"
+                + "TODO\n • adds a todo task without any date/time attached to it\n\n"
+                + "Format:\n • todo <description>\n ➡ e.g. todo read book\n\n"
+                + "DEADLINE\n • adds a deadline task that needs to be done before a specific date/time\n\n"
+                + "Format:\n • deadline <description> /by <yyyy-MM-dd>\n"
+                + " • deadline <description> /by <yyyy-MM-dd HHmm>\n"
+                + " ➡ e.g. deadline return book /by 2025-09-09 1400\n\n"
+                + "EVENT\n • adds an event task that start at a specific date/time and ends at a specific date/time\n\n"
+                + "Format:\n • event <description> /from <yyyy-MM-dd> /to <yyyy-MM-dd>\n"
+                + " • event <description /from <yyyy-MM-dd HHmm> /to <yyyy-MM-dd HHmm>\n"
+                + " ➡ e.g. event party /from 2025-12-12 1700 /to 2025-12-12 2100\n\n";
+    }
+
+    private String displayMarkHelp() {
+        return "MARK\n • mark a task as done using its respective index number in the list of tasks\n\n"
+                + "Format:\n • mark <index>\n ➡ e.g. mark 1\n\n";
+    }
+
+    private String displayUnmarkHelp() {
+        return "UNMARK\n • unmark a task as not done using its respective index number in the list of tasks\n\n"
+                + "Format:\n • unmark <index>\n ➡ e.g. unmark 2\n\n";
+    }
+
+    private String displayDeleteHelp() {
+        return "DELETE\n • delete a task from the list of tasks using its respective index number\n\n"
+                + "Format:\n • delete <index>\n ➡ e.g. delete 1\n\n";
+    }
+
+    private String displayFindHelp() {
+        return "FIND\n • find a task by searching for a keyword in the task description\n\n"
+                + "Format:\n • find <keyword>\n ➡ e.g. find book\n\n";
+    }
+
+    private String displayByeHelp() {
+        return "BYE\n • exit and close chatbot\n\n"
+                + "Format: bye\n\n";
     }
 }
